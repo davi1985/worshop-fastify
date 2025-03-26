b9cea0d0a8ab512079aeb2c4eeda5b27d34b97de
Davi Silva <davisilvaphoto@gmail.com>
2025-03-26 07:51:12 -0300

    init fastify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello Fastify');
+import Fastify from 'fastify';
+
+const fastify = Fastify();
+
+async function main() {
+  try {
+    const host = await fastify.listen({ port: 3000 });
+    console.log(`> Server started at ${host}`);
+  } catch (err) {
+    console.log({ err });
+  }
+}
+
+main();
