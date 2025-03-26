ba185eeb8876b4f488f14007db9aa44a4e584491
Davi Silva <davisilvaphoto@gmail.com>
2025-03-26 07:56:45 -0300

    creating routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import Fastify from 'fastify';
 
 const fastify = Fastify();
 
+fastify.route({
+  method: 'GET',
+  url: '/teste',
+  handler: () => ({
+    hello: 'rota de test',
+  }),
+});
+
+fastify.get('/outro-teste', () => ({ hello: 'outro teste' }));
+
 async function main() {
   try {
     const host = await fastify.listen({ port: 3000 });
